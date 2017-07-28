bcfdd5444d968f9c661260853242434cb5e1261c
Kathrin Hanauer <kal@xaikal.org>
2017-07-28 15:04:21 +0200

    Fixed spelling in copyright header.
    Signed-off-by: Kathrin Hanauer <kal@xaikal.org>

--- src/laflatex.py
+++ src/laflatex.py
@@ -3,9 +3,9 @@
 #
 # Copyright (C) 2017 : Kathrin Hanauer
 #
-# This file is part of LaFlaTex.
+# This file is part of LaFlaTeX.
 #
-# LaFlaTex is free software: you can redistribute it and/or modify
+# LaFlaTeX is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
